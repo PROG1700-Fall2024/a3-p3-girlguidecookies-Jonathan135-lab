6310ea9644e0ee65ce8c0e3b56cb08b869e6a068
Jonathan135-lab <tengjt101@gmail.com>
2024-11-20 20:27:04 -0400

    Add files via upload
    My 3rd program assignment

--- A3-P3-GirlGuideCookies.py
+++ A3-P3-GirlGuideCookies.py
@@ -10,13 +10,45 @@
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
 
+    num_guides = int(input("Enter the number of guides selling cookies: "))
+    print()
+    guides_sold = []
+    guides_name = []
 
+    for i in range(num_guides):
+        name = input(f"Enter the name of the guide #{i + 1}: ")
+        guides_name.append(name)
+        boxes_sold = int(input(f"Enter the number of boxs sold by {name}: "))
+        guides_sold.append(boxes_sold)
+        print()
+        
+    total_boxes = sum(guides_sold) 
+    average_boxes = total_boxes / num_guides
+    print()
+    print(f"The average number of bixes sold by each guide was {average_boxes:.1f}\n")
+    print("Guide\t\tPrizes Won: ")
+    print("------------------------------------------------------------")
 
+    max_boxes_sold = max(guides_sold)
+    max_boxes_sold_index = guides_sold.index(max_boxes_sold)
 
+    for i in range(num_guides):
+        name = guides_name[i]
+        boxes_sold = guides_sold[i]
 
+        if i == max_boxes_sold_index:
+            prize = "Trip to Girl Guide Jamboree in Aruba!"
+        elif boxes_sold == 0:
+            prize = " "
+        elif boxes_sold > 1 and boxes_sold < average_boxes: 
+            prize = "Left over cookies" 
+        elif boxes_sold > average_boxes:
+            prize = "Super Seller badge"
+        else:
+            prize = " "
 
-
-
+        print(f"{name}\t\t-{prize}")
+    
     # YOUR CODE ENDS HERE
 
 main()
